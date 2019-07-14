fd6c62c3ce7bf4490f0232db9e5a9a79015c3360
github username <github email address>
2019-07-13 22:21:05 -0600

    Adding devise auth routes, adding auth controller, added devise to application controller, moved user controller under api_v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  post 'api/v1/auth/request', to:'api/v1/authorization#get_authorization'
+  mount_devise_token_auth_for 'User', at: '/api/v1/auth', skip: [:omniauth_callbacks]
+  
   namespace :api do
     namespace :v1 do 
       resources :users
