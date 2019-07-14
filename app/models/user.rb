bd45f7b6089b14468d55c2320dd5fd2807d5aef4
github username <github email address>
2019-07-13 22:14:34 -0600

    Adding devise and create_user_for_google method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
-class User < ApplicationRecord
-end
+class User < ActiveRecord::Base
+    extend Devise::Models
+    devise :database_authenticatable, :registerable,
+           :rememberable, :trackable, :validatable
+    include DeviseTokenAuth::Concerns::User
+    has_secure_password
+  
+    def self.create_user_for_google(data)                  
+      where(uid: data["email"]).first_or_initialize.tap do |user|
+        user.provider="google_oauth2"
+        user.uid=data["email"]
+        user.email=data["email"]
+        user.password=Devise.friendly_token[0,20]
+        user.password_confirmation=user.password
+        user.save!
+      end
+    end
+  
+  end
