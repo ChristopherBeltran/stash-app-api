c8d112ad040f683be56d4e87c47a93a33a8f59c5
github username <github email address>
2019-10-04 22:20:15 -0600

    Fix for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://stashapp.herokuapp.com'
+     origins '*'
 
-     resource 'https://the-stash-app.herokuapp.com',
+     resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head],
        credentials: true
