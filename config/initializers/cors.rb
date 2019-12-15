5de9647084da7d402523c58cc5aded1ade9b07da
github username <christod.beltran@gmail.com>
2019-12-15 11:54:23 -0700

    Fixing cors origins for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-    origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
+    origins 'https://stashapp.herokuapp.com', 'http://localhost:3001'
      resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head],
