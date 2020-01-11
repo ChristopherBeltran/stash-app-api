e9ea89ebd225c142ffe174cc848d54a779521663
github username <christod.beltran@gmail.com>
2020-01-11 16:26:17 -0700

    Adding in password_confirmation parameter to user params on signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -53,6 +53,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
