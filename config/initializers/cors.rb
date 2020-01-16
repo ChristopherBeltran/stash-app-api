25520206cf1d9ec4dff84a39033a1b81a2f8cf9b
github username <christod.beltran@gmail.com>
2020-01-16 11:49:32 -0700

    Adding http to origin:

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-    origins 'https://stashapp.herokuapp.com', 'http://localhost:3001'
+    origins 'https://stashapp.herokuapp.com', 'http://localhost:3001', 'http://stashapp.herokuapp.com'
      resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head],
