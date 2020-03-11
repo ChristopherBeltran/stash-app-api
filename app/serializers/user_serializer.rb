9917b4968d9939bf70d21d9892742cd6101c95dd
github username <christod.beltran@gmail.com>
2020-03-11 16:47:59 -0600

    Adding dark mode column to users table as boolean, adding dark mode as attribute to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :email
+  attributes :name, :email, :dark_mode
   has_one :stream
   has_one :stash
   #has_many :articles, through: :stashes
